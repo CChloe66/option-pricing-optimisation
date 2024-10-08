67df666007bb495e0a5f984fefd0f30da6f7a464
CCurie <42432802+CChloe66@users.noreply.github.com>
2024-10-09 08:53:53 +1100

    feat: Add the object oriented implementation for cpu

---
+++ src_cpu/PayOff.h
@@ -0,0 +1,42 @@
+#ifndef PAYOFF_H
+#define PAYOFF_H
+
+
+
+#include <algorithm> // This is needed for the std::max comparison function, used in the pay-off calculations
+
+class PayOff {
+  protected : 
+    double _E; //Strike
+
+  public :
+    PayOff(); // Default (no parameter) constructor 
+    virtual ~PayOff() {}; // Virtual destructor
+
+    // // Overloaded () operator, turns the PayOff into an abstract function object
+    // virtual double operator() (const double& S) const = 0;
+
+    // virtual pure compute function  - fixed strike
+    virtual double computeFixed(const double& mean) const = 0; 
+    
+    // virtual pure compute function - floated strike
+    virtual double computeFloated(const double& mean, const double& S) const = 0;   
+};
+
+class PayOffCall : public PayOff { 
+
+    public :
+        PayOffCall ( const double& E) ; 
+        virtual ~PayOffCall () {};
+        
+        // surcharge of the compute function - fixed strike
+        virtual double computeFixed(const double& mean) const; 
+          // surcharge of the compute function - floated strike
+        virtual double computeFloated(const double& mean, const double& S) const; 
+        
+    
+
+};
+
+
+#endif // PAYOFF_H
